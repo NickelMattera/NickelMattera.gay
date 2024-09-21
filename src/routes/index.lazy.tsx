f1231838b5efbd862d08e6d2a56c43df00a2063b
Nickel Mattera <me@nicholemattera.gay>
2024-09-21 17:34:15 -0500

    chore: fix formatting issues

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -24,7 +24,7 @@ export const Route = createLazyFileRoute('/')({
       </Section>
 
       <Section title="Experience">
-        <div className={cn('mt-4 grid gap-4 items-start lg:grid-cols-2')}>
+        <div className={cn('mt-4 grid items-start gap-4 lg:grid-cols-2')}>
           <Experience
             company="Nearpod / Renaissance Learning"
             duration="July 2023 - Present"
@@ -45,7 +45,10 @@ export const Route = createLazyFileRoute('/')({
             title="Inclusion Council Member">
             <ul className={cn('list-disc pl-6')}>
               <li>Engage in monthly meetings to discuss subcommittee progress.</li>
-              <li>Contribute to the education and development subcommittee by helping to source educational material from LinkedIn Learning.</li>
+              <li>
+                Contribute to the education and development subcommittee by helping to source educational material from
+                LinkedIn Learning.
+              </li>
               <li>Collaborate on the creation of a virtual privilege walk for leaders.</li>
             </ul>
           </Experience>
@@ -87,35 +90,53 @@ export const Route = createLazyFileRoute('/')({
           </Experience>
           <Experience company="Cision" duration="September 2016 - June 2020" title="Software Engineer">
             <ul className={cn('list-disc pl-6')}>
-              <li>Contributed as a full stack engineer  working in C# and Angular.js to the Analytics Dashboard, Social Analytics Tracking, and Earned/Placed Impact areas of Cision Communication Cloud.</li>
+              <li>
+                Contributed as a full stack engineer working in C# and Angular.js to the Analytics Dashboard, Social
+                Analytics Tracking, and Earned/Placed Impact areas of Cision Communication Cloud.
+              </li>
               <li>Assisted as a frontend engineer working in React to the Email Marketing are of Trendkite.</li>
             </ul>
           </Experience>
           <Experience company="Mindtree" duration="January 2014 - September 2016" title="Software Engineer">
             <ul className={cn('list-disc pl-6')}>
-              <li>Led mobile projects, such as Volvo Scout Master and Mack CV360 iPad apps, for their Delivery Center in Gainesville, FL.</li>
-              <li>Responsible for estimations, creating build processes, meeting with clients, managing code quality, conducting technical interviews, and training junior engineers.</li>
+              <li>
+                Led mobile projects, such as Volvo Scout Master and Mack CV360 iPad apps, for their Delivery Center in
+                Gainesville, FL.
+              </li>
+              <li>
+                Responsible for estimations, creating build processes, meeting with clients, managing code quality,
+                conducting technical interviews, and training junior engineers.
+              </li>
               <li>Helped clients adapt to agile methodology</li>
             </ul>
           </Experience>
           <Experience company="Zgraph" duration="November 2011 - January 2014" title="Lead Internet Engineer">
             <ul className={cn('list-disc pl-6')}>
-              <li>Lead engineer overseeing projects such as Monterey Boats iPad app and National Marker Company Ecommerce website.</li>
+              <li>
+                Lead engineer overseeing projects such as Monterey Boats iPad app and National Marker Company Ecommerce
+                website.
+              </li>
               <li>Responsible to maintaining and upkeep of the servers hosting all of the clients websites.</li>
             </ul>
           </Experience>
           <Experience company="PosiMotion" duration="June 2008 - November 2011" title="Lead Programmer">
             <ul className={cn('list-disc pl-6')}>
-              <li>Lead engineer overseeing projects such as National Geographic's Jigsaw and Spot It games, and the 2009 Toys R Us Big Book app.</li>
+              <li>
+                Lead engineer overseeing projects such as National Geographic's Jigsaw and Spot It games, and the 2009
+                Toys R Us Big Book app.
+              </li>
               <li>Designed and led the project for G-Park, featured in an Apple iPhone 3G commercial.</li>
-              <li>Acted as a mentor to other engineers, providing support and guidance, and collaborated with individuals and companies for application publishing.</li>
+              <li>
+                Acted as a mentor to other engineers, providing support and guidance, and collaborated with individuals
+                and companies for application publishing.
+              </li>
             </ul>
           </Experience>
         </div>
       </Section>
 
       <Section title="Education">
-        <div className={cn('mt-4 grid gap-4 items-start lg:grid-cols-2')}>
+        <div className={cn('mt-4 grid items-start gap-4 lg:grid-cols-2')}>
           <Education
             title="AS, Computer Programming and Analysis"
             organization="Daytona State College"
@@ -126,7 +147,7 @@ export const Route = createLazyFileRoute('/')({
       </Section>
 
       <Section title="Certifications">
-        <div className={cn('mt-4 grid gap-4 items-start lg:grid-cols-2')}>
+        <div className={cn('mt-4 grid items-start gap-4 lg:grid-cols-2')}>
           <Certification issued="July 2020" title="Pendo Essentials">
             <p>
               This certifies that I can install Pendo, tag features, and pages, create and use segments, implement
